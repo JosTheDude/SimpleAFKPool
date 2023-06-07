7085f1649955ae97c63d9d7c2754bdd3deb09062
Jos (BM) <barnav.mitra@icloud.com>
2023-06-06 23:09:35 -0400

    Fixing Commands

--- src/main/java/xyz/joscodes/simpleafkpool/SimpleAFKPool.java
+++ src/main/java/xyz/joscodes/simpleafkpool/SimpleAFKPool.java
@@ -5,6 +5,8 @@ import com.sk89q.worldedit.world.World;
 import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.protection.managers.RegionManager;
 import com.sk89q.worldguard.protection.regions.RegionContainer;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
@@ -14,6 +16,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 
 import java.util.Objects;
+import java.util.UUID;
 
 public class SimpleAFKPool extends JavaPlugin {
 	private String commandRanInConsole;
@@ -31,13 +34,12 @@ public class SimpleAFKPool extends JavaPlugin {
 		int afkTime = getConfig().getInt("afkTime");
 
 		// Schedule task to check for players in the region every minute
-		getServer().getScheduler().runTaskTimerAsynchronously(this, () -> {
+		getServer().getScheduler().runTaskTimer(this, () -> {
 			player = checkRegionPlayers();
+			if (player != null) {
+				Bukkit.dispatchCommand(Bukkit.getConsoleSender(), commandRanInConsole.replace("%player%", player.getName()));
+			}
 		}, 0L, 20L * afkTime);
-
-		if (player != null) {
-			Bukkit.dispatchCommand(Bukkit.getConsoleSender(), commandRanInConsole.replace("%player%", player.getName()));
-		}
 	}
 
 	private Player checkRegionPlayers() {
@@ -58,7 +60,10 @@ public class SimpleAFKPool extends JavaPlugin {
 			ProtectedRegion region = regions.getRegion(regionName);
 
 			if (region != null && region.contains(BlockVector3.at(location.getBlockX(), location.getBlockY(), location.getBlockZ()))) {
-				player.sendMessage(ChatColor.translateAlternateColorCodes('&', afkMessage));
+				if (afkMessage != null) {
+					afkMessage = ChatColor.translateAlternateColorCodes('&', afkMessage);
+					player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(afkMessage.replace("%player%", player.getName())));
+				}
 				return player;
 			}
 		}
